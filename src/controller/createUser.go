363222f93e218ee43ae0e31804e503562973031a
Guilherme Sassi <dsrlikeaboss@gmail.com>
2026-02-06 14:25:16 -0300

    feat: Adicionado os causes nos erros de validação

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,23 +2,34 @@ package controller
 
 import (
 	"fmt"
-	"quentinha_golang/src/configuration/rest_err"
+	"log"
+	"net/http"
+	"quentinha_golang/src/configuration/validation"
 	"quentinha_golang/src/controller/model/request"
+	"quentinha_golang/src/controller/model/response"
 
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+	log.Println("Creating user...")
 
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect filds, error=%s\n", err.Error()))
+		log.Printf("Error trying to marshal object, error%s\n", err.Error())
+		errRest := validation.ValidateUserError(err)
 
-		c.JSON(restErr.Code, restErr)
+		c.JSON(errRest.Code, errRest)
 		return
 	}
+	
 	fmt.Println(userRequest)
-
+	response := response.UserResponse{
+		ID:    "teste",
+		Name:  userRequest.Name,
+		Email: userRequest.Email,
+		Age:   userRequest.Age,
+	}
+	c.JSON(http.StatusOK, response)
 }
