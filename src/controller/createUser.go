8f706213f0fd443327dd964bec3f010892a57429
Guilherme Sassi <dsrlikeaboss@gmail.com>
2026-02-06 15:18:18 -0300

    feat: Padronizado os loggers

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,30 +1,36 @@
 package controller
 
 import (
-	"fmt"
-	"log"
 	"net/http"
+	"quentinha_golang/src/configuration/logger"
 	"quentinha_golang/src/configuration/validation"
 	"quentinha_golang/src/controller/model/request"
 	"quentinha_golang/src/controller/model/response"
 
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CreateUser(c *gin.Context) {
-	log.Println("Creating user...")
+	logger.Info("Init CreateUser controller",
+		zap.String("journey", "createUser"),
+	)
 
 	var userRequest request.UserRequest
 
 	if err := c.ShouldBindJSON(&userRequest); err != nil {
-		log.Printf("Error trying to marshal object, error%s\n", err.Error())
+		logger.Error("Error trying to marshal object", err,
+			zap.String("controllers", "createUser"))
+
 		errRest := validation.ValidateUserError(err)
 
 		c.JSON(errRest.Code, errRest)
 		return
 	}
-	
-	fmt.Println(userRequest)
+
+	logger.Info("User created successfully",
+		zap.String("journey", "createUser"),
+	)
 	response := response.UserResponse{
 		ID:    "teste",
 		Name:  userRequest.Name,
